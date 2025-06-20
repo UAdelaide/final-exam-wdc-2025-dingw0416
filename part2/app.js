4f44fc7923e4abac3f65a10b6a84cb601b61919e
dingw0416 <dingw0416@gmail.com>
2025-06-20 13:04:16 +0000

    [automatic commit] 2025-06-20 13:04:16 UTC 60.225.213.251

--- part2/app.js
+++ part2/app.js
@@ -20,7 +20,7 @@ app.post('/login', async (req, res) => {
   const { username, password } = req.body;
   try {
     const [rows] = await pool.query(
-      'SELECT * FROM Users WHERE username = ? AND password_',
+      'SELECT * FROM Users WHERE username = ? AND password_hash = ',
       [username]
     );
 
