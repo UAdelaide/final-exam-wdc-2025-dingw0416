8a591b8f08d959d142f541730979c68509561a37
dingw0416 <dingw0416@gmail.com>
2025-06-20 12:57:45 +0000

    [automatic commit] 2025-06-20 12:57:45 UTC 60.225.213.251

--- part2/app.js
+++ part2/app.js
@@ -33,11 +33,11 @@ app.post('/login', async (req, res) => {
       }
 
       if (user.role === 'walker') {
-       return res.redirect('/walker-dashboard.html');
-  }
+        return res.redirect('/walker-dashboard.html');
+      }
 
-  return res.status(403).send('Unknown role');
-}
+      return res.status(403).send('Unknown role');
+    }
 
 return res.status(401).send('Invalid credentials');
 
