1af7a781961c561914e9b1dc398e75e5322eae23
dingw0416 <dingw0416@gmail.com>
2025-06-20 03:08:49 +0000

    [automatic commit] 2025-06-20 03:08:49 UTC 60.225.213.251

--- part1/app.js
+++ part1/app.js
@@ -23,7 +23,7 @@ async function main() {
   }
 
   const app = express();
-
+  const path = require('path');
   app.use(express.json());
   app.use(express.static(path.join(__dirname, 'public')));
 
