e6c0b2101c63222265746c215db9ec7ad6321652
dingw0416 <dingw0416@gmail.com>
2025-06-20 11:55:55 +0000

    [automatic commit] 2025-06-20 11:55:54 UTC ;; communications error to 208.67.222.222#53: timed out 60.225.213.251

--- part2/dbConfig.js
+++ part2/dbConfig.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
+  host: 'localhost',
+  user: 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'dogwalks_db',
   waitForConnections: true,
